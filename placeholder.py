eddb166cdf57b3d7bd4966cf2aa6e4b5c3a315e6
Haolin Wu <haolin97@gmail.com>
2020-01-19 14:23:05 +0800

    Add python placeholder file

---
+++ placeholder.py
@@ -0,0 +1,11 @@
+# Haolin Wu, <date>
+# LeetCode <problem> <problem_url>
+
+
+class Solution:
+	def Solution(self, s: str) -> int:
+		return int(s)
+
+
+if __name__ == "__main__":
+	solution = Solution()
