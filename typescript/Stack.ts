ab62e4df50b550cbc3222d988615607ba3efbe05
Haolin Wu <haolin@haolin.dev>
2025-05-22 22:33:49 +0800

    Add simple stack class

--- typescript/Stack.ts
+++ typescript/Stack.ts
@@ -1,2 +1,21 @@
-const stack: string = 'hi';
-console.log('hi');
+// TypeScript stack.
+// Last in first out. Can use built in array methods.
+class Stack {
+    private list: any[] = [];
+
+    push(element: any): void {
+        this.list.push(element);
+    }
+
+    pop(): any {
+        return this.list.pop();
+    }
+
+    peek(): any {
+        return this.list.length > 0 ? this.list[this.list.length - 1] : undefined;
+    }
+
+    isEmpty(): boolean {
+        return this.list.length > 0;
+    }
+}
