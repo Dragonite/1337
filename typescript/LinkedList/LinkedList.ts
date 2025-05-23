3eb89c09e9044ac2b9059e3f68ba19706495883e
Haolin Wu <haolin@haolin.dev>
2025-05-23 16:11:26 +0800

    Add linked list abstract and primitive singly linked list

--- typescript/LinkedList/LinkedList.ts
+++ typescript/LinkedList/LinkedList.ts
@@ -1,8 +1,8 @@
 export class ListNode {
     public value: any;
-    public next: ListNode;
+    public next: ListNode | undefined;
 
-    constructor(value: any, next: ListNode) {
+    constructor(value: any, next: ListNode | undefined = undefined) {
         this.value = value;
         this.next = next;
     }
@@ -11,16 +11,91 @@ export class ListNode {
 export abstract class LinkedList {
     head: ListNode | undefined = undefined;
     abstract addNode(value: any): void;
+    abstract removeLast(): ListNode | undefined;
     abstract findLast(): ListNode | undefined;
     length: number = 0;
 
-    removeFirst(): void {
+    removeFirst(): ListNode | undefined {
         if (!this.head) return;
+        const existingHead = this.head;
         // We need to move the pointer forward
-        if (this.head.next) {
-            this.head = this.head.next;
+        if (existingHead.next) {
+            this.head = existingHead.next;
+            return existingHead;
         } else {
             this.head = undefined;
+            return;
+        }
+    }
+
+    removeAtIndex(targetIndex: number): ListNode | undefined {
+        // Checking for index out of range, or no list.
+        if (targetIndex < 0 || targetIndex > this.length - 1 || !this.head) {
+            return;
+        }
+        // If it's index 0, looking for the first item , we should remove first and return it.
+        if (targetIndex === 0) {
+            return this.removeFirst();
+        }
+        // Indexes should now be valid. Removal means detach from previous index, attach to next index.
+        let prevNode: ListNode | undefined;
+        let currentNode: ListNode | undefined = this.head;
+        let index = 0;
+        // We only need the previous node.
+        while (index < targetIndex && currentNode) {
+            prevNode = currentNode;
+            currentNode = currentNode.next;
+            index += 1;
+        }
+        if (prevNode && currentNode) {
+            prevNode.next = currentNode.next;
+        }
+        return currentNode;
+    }
+
+    addAtIndex(value: any, targetIndex: number): ListNode | undefined {
+        if (targetIndex < 0 || targetIndex > this.length - 1) {
+            return;
+        }
+        if (!this.head) {
+            if (targetIndex === 0) {
+                this.head = new ListNode(value);
+                return;
+            } else {
+                return;
+            }
+        } else {
+            // There is an existing head.
+            let prevNode: ListNode | undefined;
+            let currentNode: ListNode | undefined = this.head;
+            let index = 0;
+            // We only need the previous node.
+            while (index < targetIndex && currentNode) {
+                prevNode = currentNode;
+                currentNode = currentNode.next;
+                index += 1;
+            }
+            if (prevNode && currentNode) {
+                const newNode = new ListNode(value);
+                prevNode.next = newNode;
+                newNode.next = currentNode;
+                return newNode;
+            }
+            return;
+        }
+    }
+
+    print(): void {
+        if (!this.head) {
+            console.log([]);
+        } else {
+            const listAsArray: any[] = [];
+            let node: ListNode | undefined = this.head;
+            while (node) {
+                listAsArray.push(node.value);
+                node = node.next;
+            }
+            console.log(listAsArray);
         }
     }
 }
--- typescript/LinkedList/SinglyLinkedList.ts
+++ typescript/LinkedList/SinglyLinkedList.ts
@@ -1,11 +1,49 @@
 import { LinkedList, ListNode } from "./LinkedList";
 
 // Linked List with no pointers, basically every list item is unknown. Also unknown length.
+// Add node: O(n)
+// find last: O(n)
+// remove last: O(n)
 class PrimitiveLinkedList extends LinkedList {
     addNode(value: any): void {
+        if (this.head) {
+            const lastNode = this.findLast();
+            if (lastNode) {
+                lastNode.next = new ListNode(value);
+            }
+        } else {
+            this.head = new ListNode(value);
+        }
+        this.length += 1;
         return;
     }
+
     findLast(): ListNode | undefined {
-        return;
+        if (!this.head) return;
+        let node: ListNode = this.head;
+        while (node && node.next) {
+            node = node.next;
+        }
+        return node;
+    }
+
+    removeLast() : ListNode | undefined {
+        return this.removeAtIndex(this.length - 1);   
     }
 }
+
+const list = new PrimitiveLinkedList();
+list.addNode(1);
+console.log(list.length)
+list.addNode(2);
+list.addNode(3);
+list.addNode(4);
+list.addNode(5);
+const removed = list.removeAtIndex(3);
+console.log(removed)
+list.print();
+const added = list.addAtIndex(4, 3);
+console.log(added);
+list.print();
+list.removeLast();
+list.print();
